8aec5aa4aa88a94b6ccefa65eb2bed275c7cf9e9
laks1408 <laksplaysmc@gmail.com>
2025-01-12 12:04:15 +0700

    main menu fix + girl customer

--- Assets/Scripts/Manager/LifeManager.cs
+++ Assets/Scripts/Manager/LifeManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class LifeManager : MonoBehaviour
 {
@@ -119,5 +120,8 @@ public class LifeManager : MonoBehaviour
         }
 
         Application.Quit();
+        //SceneManager.LoadScene(1);
     }
+
+
 }
