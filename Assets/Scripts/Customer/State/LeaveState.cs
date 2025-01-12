b5b48ce9077e20071aae3239bce0a3b3ca6abda0
laks1408 <laksplaysmc@gmail.com>
2025-01-12 14:01:22 +0700

    customer delay

--- Assets/Scripts/Customer/State/LeaveState.cs
+++ Assets/Scripts/Customer/State/LeaveState.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Threading.Tasks;
+
 
 public class LeaveState : ICustomerState
 {
     float LeaveTime = 0f;
+    public Sprite newSprite;
+
     public void EnterState(Customer customer)
     {
         if(customer.isServed) {
             GameManager.Instance.UpdateScore();
+            customer.Leave();
         } else {
             LifeManager.Instance.ReduceLife();
+            //ChangeSprite(customer.gameObject, newSprite);
+            LeaveAfterDelayAsync(customer);
         }
-        customer.Leave();
+
     }
 
     public void ExitState(Customer customer)
@@ -23,5 +30,26 @@ public class LeaveState : ICustomerState
     public void UpdateState(Customer customer)
     {
         
+    }
+
+    private void ChangeSprite(GameObject targetObject, Sprite newSprite)
+    {
+        SpriteRenderer spriteRenderer = targetObject.GetComponent<SpriteRenderer>();
+
+        if (spriteRenderer != null)
+        {
+            spriteRenderer.sprite = newSprite;
+        }
+        else
+        {
+            Debug.LogWarning("SpriteRenderer not found on target object!");
+        }
+    }
+
+    private async void LeaveAfterDelayAsync(Customer customer)
+    {
+        
+        await Task.Delay(1000);
+        customer.Leave();
     }
 }
